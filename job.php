efe09cd6524abe58292bedc8771c1f790ba1a4d6
CuongCung0401 <duycuong4tml@gmail.com>
2025-03-18 11:49:51 +0700

    job

--- job.php
+++ job.php
@@ -192,6 +192,243 @@
     </table>
 </section>
 
+<!-- Job 3:  Devops Engineer -->
+<h2>JOB TITLE: DEVOPS ENGINEER, DIGITAL BUSINESS| J0098</h2>
+<section>
+    <h3>JOB DESCRIPTION & REQUIREMENTS</h3>
+    <table class="job-table">
+        <tr>
+            <th colspan="2">Job Overview</th>
+        </tr>
+        <tr>
+            <td><strong>Description</strong></td>
+            <td>We are looking for a DevOps Engineer to deploy, manage, and optimize infrastructure across on-premises, AWS, GCP, and VNG Cloud. The role focuses on CI/CD, automation, system reliability, and performance tuning for high-scale business products.</td>
+        </tr>
+        <tr>
+            <td><strong>Responsibilities</strong></td>
+            <td>
+                <ul>
+                    <li>Working on the line between Software Engineering and Systems Engineering.</li>
+                    <li>Deploy and maintain the full product ecosystem across on-premises, cloud (AWS, GCP, VNG Cloud), including customer on-prem environments.</li>
+                    <li>Troubleshoot and resolve infrastructure-related issues proactively.</li>
+                    <li>Design, automate, and optimize CI/CD pipelines to ensure fast, reliable, and secure software delivery (e.g. Jenkins, GitlabCI, ArgoCD).</li>
+                    <li>Enhance engineering productivity by develop observability tools for real-time monitoring, alerting, and log aggregation (e.g. ELK, Prometheus, Grafana).</li>
+                    <li>Monitor, manage and maintain infrastructure to ensure high availability, performance, and security of critical services.</li>
+                    <li>Implement auto-scaling, failover strategies, and disaster recovery plans.</li>
+                    <li>Collaborate with our product engineers to deliver scalable and efficient solutions.</li>
+                    <li>Design and manage large system of real-world business products.</li>
+                    <li>Work with experts in Data Science field.</li>
+                    <li>Handle end-to-end tasks in various big scale projects.</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Requirements</th>
+        </tr>
+        <tr>
+            <td><strong>Essential</strong></td>
+            <td>At least 2 years of hands-on experience as a DevOps or Site Reliability Engineering (SRE).</td>
+        </tr>
+        <tr>
+            <td><strong>Skills</strong></td>
+            <td>
+            Hands-on experience with Docker & Kubernetes.
+            </td>
+            <td>
+            Experience managing Linux operating systems
+            </td>
+            <td>
+            Experience managing cloud infrastructure (AWS, GCP, VNG Cloud).
+            </td>
+            <td>
+            Working knowledge of design procedures for system troubleshooting and maintenance.
+            </td>
+            <td>
+            Hands-on experience with computer vision AI, including image/video processing, object tracking, and recognition. 
+            </td>
+        </tr>
+        <tr>
+            <td><strong>Preferable Skills</strong></td>
+            <td>
+                <ul>
+                    <li>Experience with Stable Diffusion for AI-generated content</li>
+                    <li>Experience in cloud-based AI model deployment at scale.</li>
+        
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Additional Info</th>
+        </tr>
+        <tr>
+            <td><strong>Average Salary</strong></td>
+            <td>$127.3k USD per annum (<a href="https://www.indeed.com/career/devops-engineer/salaries">Source</a>)</td>
+        </tr>
+        <tr>
+            <td><strong>Apply</strong></td>
+            <td><a href="apply.php">Click Here</a></td>
+        </tr>
+    </table>
+</section>
+
+<!-- Job 4:  Senior AI engineer -->
+<h2>JOB TITLE: SENIOR AI ENGINEER - GAME DEVELOPMENT| J0099</h2>
+<section>
+    <h3>JOB DESCRIPTION & REQUIREMENTS</h3>
+    <table class="job-table">
+        <tr>
+            <th colspan="2">Job Overview</th>
+        </tr>
+        <tr>
+            <td><strong>Description</strong></td>
+            <td>We are hiring a Senior AI Engineer to develop AI-driven computer vision solutions for game development. This role requires experience in developing and deploying AI models in real-world applications, with a focus on launching AI-powered solutions and contributing to successful AI-driven projects.</td>
+        </tr>
+        <tr>
+            <td><strong>Responsibilities</strong></td>
+            <td>
+                <ul>
+                    <li>Research and apply ML/DL algorithms to enhance game AI and computer vision-based systems. </li>
+                    <li>Work closely with engineers to integrate AI-driven computer vision models into game development workflows. </li>
+                    <li>Develop and optimize real-time vision-based AI models for object detection, tracking, and scene understanding. </li>
+                    <li>Stay updated with the latest AI, GenAI, and Computer Vision advancements. </li>
+                    <li>Optimize and deploy AI solutions using Nvidia frameworks (Deepstream, Triton Inference Server, etc.) and cloud platforms.</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Requirements</th>
+        </tr>
+        <tr>
+            <td><strong>Essential</strong></td>
+            <td>At least 3 years of hands-on experience in developing and deploying AI models.</td>
+        </tr>
+        <tr>
+            <td><strong>Skills</strong></td>
+            <td>
+            Proven track record of AI-powered products, published research, or real-world applications. 
+            </td>
+            <td>
+            Bachelor's degree in Computer Science, Information Technology, or related fields. 
+            </td>
+            <td>
+            Strong expertise in Python and backend development (Django or other frameworks). 
+            </td>
+            <td>
+            Deep knowledge of ML/DL frameworks like TensorFlow, PyTorch, MXNet, Pandas, PySpark. 
+            </td>
+            <td>
+            In-depth understanding of UNIX/ Linux system internals, TCP/ IP networking, load balancers, web servers, caching.
+            </td>
+            <td>
+            Well-versed in scripting (Bash/ Python) and infrastructure automation - not limit to Terraform/Ansible.
+            </td>
+            <td>
+            Experience with Jenkins, GitLab, ELK, or equivalent technology stack.
+            </td>
+            
+        </tr>
+        <tr>
+            <td><strong>Preferable Skills</strong></td>
+            <td>
+                <ul>
+                    <li>Knowledge in DevSecOps is a plus.</li>
+                    <li>Extensive experience in distributed systems, databases, and performance analysis.</li>
+        
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Additional Info</th>
+        </tr>
+        <tr>
+            <td><strong>Average Salary</strong></td>
+            <td>$168.6k USD per annum (<a href="https://www.indeed.com/career/senior-ai%2Fml-engineer/salaries">Source</a>)</td>
+        </tr>
+        <tr>
+            <td><strong>Apply</strong></td>
+            <td><a href="apply.php">Click Here</a></td>
+        </tr>
+    </table>
+</section>
+
+<!-- Job 5:  Data Scientist -->
+<h2>JOB TITLE: DATA SCIENTIST - VNGGAMES| J0100</h2>
+<section>
+    <h3>JOB DESCRIPTION & REQUIREMENTS</h3>
+    <table class="job-table">
+        <tr>
+            <th colspan="2">Job Overview</th>
+        </tr>
+        <tr>
+            <td><strong>Description</strong></td>
+            <td>We are looking for a Data Scientist to join our Data Science Team. The DS team will work on developing advanced machine learning products and supporting key business initiatives with a focus on game users and game performance, platform data such as: churn prediction, recommendation system, fraud/anomaly detection, user segmentation, user’s LTV, targeted advertising, etc. </td>
+        </tr>
+        <tr>
+            <td><strong>Responsibilities</strong></td>
+            <td>
+                <ul>
+                    <li>In charge of developing POC and deploying model into production as well as AB testing.</li>
+                    <li>WExtract actionable business insights from data, formulate business solutions based on data to improve the company's business.</li>
+                    <li>Analyze on large amounts of data from various sources to communicate insights, findings and recommendations to stakeholders.</li>
+                    <li>Identify opportunities for process improvement, automation, and innovation to enhance data analysis capabilities.  </li>
+                    <li>Utilize statistical and machine learning techniques to identify patterns and trends in data.</li>
+                    <li>Effective communication with leadership, tech and business teams.</li>
+                    <li>Involve in the development and evolvement of the data product.</li>
+                    <li>Help junior and intermediate members enhance their careers and their skill sets in data science.</li>
+                    <li>Using cutting-edge approaches and procedures, as well as developing new ones, when necessary, support the redesign of the data science model development lifecycle</li>
+                    <li>Communicating complex analytical findings and recommendations to non-technical stakeholders, including senior management.</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Requirements</th>
+        </tr>
+        <tr>
+            <td><strong>Essential</strong></td>
+            <td>Minimum Bachelor's degree in Computer Science, Data Science, Statistics, Applied Mathematics, Econometrics, or a relevant field of study. At least 3+ years of experience in data analytics and data science. </td>
+        </tr>
+        <tr>
+            <td><strong>Skills</strong></td>
+            <td>
+            Strong analytical skills: capable of collecting, organizing, and analyzing significant amounts of information with attention to detail and accuracy. 
+            </td>
+            <td>
+            Proficiency in SQL, Python, and machine learning frameworks (such as Scikit-learn, TensorFlow, PyTorch). 
+            </td>
+            <td>
+            Solid background in machine learning/statistics with hands-on experience in academia and/or industry. 
+            </td>
+            <td>
+            Excellent communication and collaboration skills, demonstrating a proven ability to work effectively in cross-functional teams. 
+            </td>
+            <td>
+            A keen learner and self-motivated team player with the ability to handle multiple projects simultaneously. 
+            </td>
+            
+        </tr>
+        <tr>
+            <td><strong>Preferable Skills</strong></td>
+            <td>
+                <ul>
+                    <li>Possess a good business mindset.</li>
+        
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <th colspan="2">Additional Info</th>
+        </tr>
+        <tr>
+            <td><strong>Average Salary</strong></td>
+            <td>$126.6k USD per annum (<a href="https://www.indeed.com/career/data-scientist/salaries">Source</a>)</td>
+        </tr>
+        <tr>
+            <td><strong>Apply</strong></td>
+            <td><a href="apply.php">Click Here</a></td>
+        </tr>
+    </table>
+</section>
+
 <?php   
     include 'footer.inc';
 ?>
